6e4c1fca895ce45fefef9474d83bc66651994eb4
yunhandofficial <pors356a@gmail.com>
2019-10-24 15:35:35 +0900

    debug false

--- insta/settings.py
+++ insta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*yj_m@_c_47dp69y&a%1huqoj764qd@681je_xv)b%b2k2p$i+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
